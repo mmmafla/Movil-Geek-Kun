14b6d2bcc90bba79328ac4aa09e81229d470a64e
benjamin <133883483+mmmafla@users.noreply.github.com>
2024-08-29 13:44:36 -0400

    Creacion de paginas
    Se crearon paginas de la estructura basica de la aplicacion movil, tal como: Inicio de aplicacion, Login y Registro, y Menu de inicio

--- Geek-Kun/src/app/app-routing.module.ts
+++ Geek-Kun/src/app/app-routing.module.ts
@@ -4,12 +4,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./pages/main/main.module').then( m => m.MainPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   },
 ];
 
