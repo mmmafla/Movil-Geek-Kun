6d1c29d1217277a9d68a1b8c58c7c78254803cbb
benjamin <133883483+mmmafla@users.noreply.github.com>
2024-09-09 23:27:22 -0300

    animacion de noticia
    Se implemento animacion en la primera noticia presente

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,45 @@
-import { Component, ViewChild } from '@angular/core';
-import { IonContent } from '@ionic/angular';
+import { Component, ViewChildren, ViewChild, ElementRef, OnInit, QueryList, } from '@angular/core';
+import { AnimationController, IonCard, Animation, IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
   @ViewChild(IonContent) content!: IonContent;
 
-  constructor() {}
+  @ViewChildren(IonCard, {read: ElementRef})
+  cardElements!:QueryList<ElementRef<HTMLIonCardElement>>
+
+  @ViewChild('animarH1',{read: ElementRef, static: true})
+  animarH1!:ElementRef;
+
+  private animation!:Animation;
+  constructor(private animationController: AnimationController) { }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+    const cardD = this.animationController
+      .create()
+      .addElement(this.cardElements.get(0)!.nativeElement)
+      .duration(1500)
+      .iterations(Infinity)
+      .direction('alternate')
+      .fromTo('background', 'green', 'var(--background)'); 
+
+      this.animation = this.animationController
+      .create()
+      .duration(4000)
+      .iterations(Infinity)
+      .addAnimation([cardD]);
+
+      this.animation.play();
+
+  }
 
   scrollToTop() {
     this.content.scrollToTop(500);
