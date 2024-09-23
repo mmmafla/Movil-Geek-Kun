7d555d6b1ac6310877d76a1a373233481dffaa18
Francisco Silva <133883562+vXkn6@users.noreply.github.com>
2024-09-22 22:18:31 -0300

    Add files via upload

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonContent } from '@ionic/angular';
+import { FortniteService  } from 'src/app/services/fortnite.service';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,25 @@ import { IonContent } from '@ionic/angular';
 })
 export class HomePage {
   @ViewChild(IonContent) content!: IonContent;
+  
+  newsItems: any[] = [];
 
-  constructor() {}
+  constructor(private fortniteService: FortniteService) {}
 
+  ngOnInit() {
+    this.fortniteService.getNews().subscribe(
+      response => {
+          this.newsItems = response.news; // Asignamos la lista de noticias
+          console.log(this.newsItems); // Para depuración
+      },
+      error => {
+        console.error('Error al cargar las noticias', error);
+      }
+    );
+  }
   scrollToTop() {
     this.content.scrollToTop(500);
   }
 }
+
+
--- src/app/services/fortnite.service.ts
+++ src/app/services/fortnite.service.ts
@@ -1,86 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-// Definir las interfaces para la estructura de la respuesta
-interface ShopResponse {
-  status: number;
-  data: {
-    hash: string;
-    date: string; // o Date, dependiendo de cómo prefieras manejar las fechas
-    vbuckIcon: string;
-    entries: Entry[];
-  };
-}
-
-interface Entry {
-  regularPrice: number;
-  finalPrice: number;
-  devName: string;
-  offerId: string;
-  inDate: string; // o Date
-  outDate: string; // o Date
-  bundle?: {
-    name: string;
-    info: string;
-    image: string;
-  };
-  banner?: {
-    value: string;
-    intensity: string;
-    backendValue: string;
-  };
-  offerTag?: {
-    id: string;
-    text: string;
-    giftable: boolean;
-    refundable: boolean;
-  };
-  layout?: {
-    id: string;
-    name: string;
-    category: string;
-    index: number;
-    rank: number;
-    showIneligibleOffers: string;
-    background: string;
-    useWidePreview: boolean;
-    displayType: string;
-    textureMetadata: { key: string; value: string }[];
-    stringMetadata: { key: string; value: string }[];
-    textMetadata: { key: string; value: string }[];
-    colors: {
-      color1: string;
-      color2: string;
-      color3: string;
-      textBackgroundColor: string;
-      tileSize: string;
-    };
-    displayAssetPath: string;
-    newDisplayAssetPath: string;
-    newDisplayAsset: {
-      id: string;
-      cosmeticId: string;
-      materialInstances: { id: string; primaryMode: string; productTag: string }[];
-    };
-  };
-  // Otros campos que puedan ser necesarios...
-}
 
 @Injectable({
   providedIn: 'root'
 })
 export class FortniteService {
-
   private apiUrl = 'https://fortnite-api.com/v2/shop';
   private apiKey = 'afc78907-1dd3-428b-b17a-dd4ed8b161c3';
+  private newsApiUrl = 'https://fortniteapi.io/v1/news?lang=es&type=br';
+  private newsApiKey = '5a8f26a2-3d756271-eed41ad1-144fe5e0';
 
-  constructor(private http: HttpClient) { }
 
-  getShopItems(): Observable<ShopResponse> {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.apiKey}`
+  constructor(private http: HttpClient) {}
+
+  getShopItems(): Observable<any> {
+    return this.http.get<any>(this.apiUrl, {
+      headers: {
+        'Authorization': this.apiKey
+      }
     });
-    return this.http.get<ShopResponse>(this.apiUrl, { headers });
   }
+  getNews(): Observable<any> {
+    return this.http.get<any>(this.newsApiUrl, {
+      headers: {
+        'Authorization': this.newsApiKey
+      }
+    });
+}
 }
