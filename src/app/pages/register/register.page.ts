619d1e24a48a57ec98dc2008319f8d88a95ce9a7
benjamin <133883483+mmmafla@users.noreply.github.com>
2024-10-21 21:25:36 -0300

    Aplicacion de Plugin Nativo
    Se aplica el plugin nativo de camara dentro de la aplicacion

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-register',
@@ -10,19 +12,42 @@ import { ToastController } from '@ionic/angular';
 
 export class RegisterPage implements OnInit {
 
-  register:any={
-    Nombre:"",
-    Apellido:"",
-    Usuario:"",
-    Correo:"",
-    Contrasena:""
-  }
+  form = new FormGroup({
+    imagen: new FormControl('', [Validators.required]),
+    nombre: new FormControl('', [Validators.required]),
+    apellido: new FormControl('', [Validators.required]),
+    usuario: new FormControl('', [Validators.required]),
+    correo: new FormControl('', [Validators.required]),
+    contrasena: new FormControl('', [Validators.required]),
+  })
   
   field:string="";
 
   constructor(public router: Router,public toastController:ToastController) { }
 
+  UtilSRV = inject(UtilsService)
+
   ngOnInit() {
+  }
+
+  // Tomar o selecionar una imagen del celular.
+  async takeImage(): Promise<void> {
+    try {
+      const result = await this.UtilSRV.takePicture('Foto de Perfil');
+      console.log('Resultado de takePicture:', result); // Verifica la estructura del objeto resultante
+      const dataUrl = result?.dataUrl;
+      if (dataUrl) {
+        if (this.form && this.form.controls && this.form.controls.imagen) {
+          this.form.controls.imagen.setValue(dataUrl);
+        } else {
+          console.error('El control de imagen no está inicializado o no es accesible.');
+        }
+      } else {
+        console.error('dataUrl no está definido en el resultado.');
+      }
+    } catch (error) {
+      console.error('Error al tomar la foto:', error);
+    }
   }
 
   validateModel(model:any){
@@ -35,14 +60,16 @@ export class RegisterPage implements OnInit {
     return true;
   }
 
+  /**
   registrar(){
-    if(this.validateModel(this.register)){
+    if(this.validateModel(this.form.)){
       this.router.navigate(['/tab-componet/news']);
       this.presentToast("top","¡Bienvenido! No te pierdas las ultimas noticias, la tienda de articulos y las alerta de misiones de STW.",5000)
     }else{
       this.presentToast("middle","Falta ingresar un dato en el campo de: "+this.field);
     }    
   }
+  **/
 
   async presentToast(position: 'top' | 'middle' | 'bottom', msg:string, duration?:number) {
     const toast = await this.toastController.create({
---
+++ src/app/services/utils.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilsService {
+
+  async takePicture(promptLabelHeader: string) {
+    return await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Prompt,
+      promptLabelHeader,
+      promptLabelPhoto: 'Selecciona una imagen',
+      promptLabelPicture: 'Toma una foto'
+    });
+
+  };
+
+  constructor() { }
+
+}
