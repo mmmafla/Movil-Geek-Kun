cf8e2c9969d7637872c6ed959bf43103cdd89dfd
Francisco Silva <133883562+vXkn6@users.noreply.github.com>
2024-11-24 00:19:27 -0300

    Add files via upload

--- src/app/pages/shop/shop.page.ts
+++ src/app/pages/shop/shop.page.ts
@@ -15,10 +15,8 @@ export class ShopPage implements OnInit {
   number!: number;
 
   shopItems: any[]=[]; 
-  bundle: any[] = [];
-  seccion: any [] = [];
-  btr: any [] = [];
-  jam: any [] = [];
+  obj: any[] = [];
+  grouped: { [key: string]: any[] } = {};
 
   constructor(private fortniteShopApiService: FortniteShopApiService, private dbLocalService:DblocalService) { }
 
@@ -29,19 +27,43 @@ export class ShopPage implements OnInit {
     this.favoritos =(this.dbLocalService.mostrarBD());
     console.log(this.favoritos);
   }
+  getBackgroundImage(colors: { color1?: string, color2?: string, color3?: string }): string {
+    if (colors.color1 && colors.color2 && colors.color3) {
+      return `linear-gradient(to bottom, #${colors.color1}, #${colors.color2}, #${colors.color3})`;
+    } else if (colors.color1 && colors.color3) {
+      return `linear-gradient(to bottom, #${colors.color1}, #${colors.color3})`;
+    } else if (colors.color1) {
+      return `#${colors.color1}`;
+    } else if (colors.color3) {
+      return `#${colors.color3}`;
+    } else {
+      return '#FFFFFF';  // Color por defecto si no hay colores
+    }
+  }
+  
+
 
   ngOnInit() {
 
     this.fortniteShopApiService.getShopItems().subscribe((data: any) =>{
       this.shopItems = data.data.entries;
-      this.bundle = this.shopItems.filter(item => item.bundle?.image);
-      this.seccion = this.shopItems.filter(item => item.layout.id == 'ChampionsRoad');
-      this.jam = this.shopItems.filter(item => item.layout.name =='Jam Tracks');
-      this.btr = this.shopItems.filter(item => item.brItems);
       console.log(this.shopItems);
-      console.log(this.bundle);
+      this.obj = this.shopItems.filter(item => item.layout.name !=='Jam Tracks');
+      console.log(this.obj);
+      
+      this.obj.forEach(item => {
+        const categoryName = item.layout.name;
+        if (!this.grouped[categoryName]) {
+          this.grouped[categoryName] = []; // Inicializa el arreglo si no existe
+        }
+        this.grouped[categoryName].push(item); // Agrega el item al arreglo correspondiente
+        
+      });
+
+
     });
 
   }
 
 }
+
